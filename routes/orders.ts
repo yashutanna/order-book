2a35f301d16cf93b3c62a3680e811c7be50a6559
Yashu <yashu.tanna@outlook.com>
2021-03-08 18:47:02 +0200

    feature(Basic auth)
    Add basic authentication to POST method for creating limit orders

--- routes/orders.ts
+++ routes/orders.ts
@@ -1,10 +1,19 @@
 import { Router } from 'express';
 import { getOrderBook, getRecentTrades, placeLimitOrder } from '../services/orders/orders';
+import basicAuth from "express-basic-auth";
 
 const orderRouter = Router();
 
 // Get order book
 orderRouter.get('/', (_, res) => res.status(200).send(getOrderBook()));
+
+// Get recent trades
+orderRouter.get('/trades/recent', (_, res) => res.status(200).send(getRecentTrades()));
+
+orderRouter.use(basicAuth({
+  // TODO - add these to environment variables
+  users: { admin: "admin" }
+}));
 
 // Post limit order 
 orderRouter.post<{}, {}, LimitOrderInput>(
@@ -15,8 +24,5 @@ orderRouter.post<{}, {}, LimitOrderInput>(
     res.status(200).send(placeLimitOrder(limitOrderInput));
   }
 );
-
-// Get recent trades
-orderRouter.get('/trades/recent', (_, res) => res.status(200).send(getRecentTrades()));
 
 export default orderRouter;
